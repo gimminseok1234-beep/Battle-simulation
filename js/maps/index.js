ad981d9307097e118ac81f7422c3ff97c6145160
MSG7 <gimminseok1234@gmail.com>
2025-09-02 22:33:12 +0900

    Update index.js

--- js/maps/index.js
+++ js/maps/index.js
@@ -2,11 +2,11 @@
 // 새로운 맵을 추가하려면, 아래에 import 구문을 추가하고 localMaps 배열에 맵 변수를 추가하기만 하면 됩니다.
 
 import { grasslandSkirmishMap } from './GrasslandSkirmish.js';
-import { centralBridgeShowdownMap } from './CentralBridgeShowdown.js'; // '중앙대교 결전' 맵 추가
+// '중앙대교 요새' 맵 추가 (가져오는 변수 이름을 수정해야 합니다)
+import { centralBridgeFortressMap } from './CentralBridgeShowdown.js';
 
 // 홈 화면에 표시할 기본 맵 목록
 export const localMaps = [
-    centralBridgeShowdownMap,
+    centralBridgeFortressMap, // 배열에 추가하는 변수 이름도 수정해야 합니다.
     grasslandSkirmishMap,
 ];
-
