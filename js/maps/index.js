187f51524f13b16265916882408df6c7a95baf4c
MSG7 <gimminseok1234@gmail.com>
2025-09-04 15:53:48 +0900

    Update index.js

--- js/maps/index.js
+++ js/maps/index.js
@@ -1,12 +1,13 @@
 // js/maps/index.js
 
 import { grasslandSkirmishMap } from './GrasslandSkirmish.js';
-import { CentralBridgeShowdown } from './CentralBridgeShowdown.js';
+// 'CentralBridgeShowdown' 대신 'centralBridgeFortressMap'으로 가져옵니다.
+import { centralBridgeFortressMap } from './CentralBridgeShowdown.js';
 import { RuinsOfValor } from './RuinsOfValor.js';
 
-// gameManager.js에서 localMaps로 import 하므로 변수 이름을 수정합니다.
+// GameManager에서 'localMaps'를 사용하므로 export 이름을 수정하고, 배열 형태로 만듭니다.
 export const localMaps = [
     grasslandSkirmishMap,
-    CentralBridgeShowdown,
+    centralBridgeFortressMap, // 가져온 이름 그대로 사용합니다.
     RuinsOfValor,
 ];
