b8286e26585dc2171578dd107cfcc0f9be890117
MSG7 <gimminseok1234@gmail.com>
2025-09-04 15:45:21 +0900

    Update index.js

--- js/maps/index.js
+++ js/maps/index.js
@@ -1,12 +1,12 @@
 // js/maps/index.js
 
-import { GrasslandSkirmish } from './GrasslandSkirmish.js';
+import { grasslandSkirmishMap } from './GrasslandSkirmish.js'; // 수정: grasslandSkirmishMap으로 가져옵니다.
 import { CentralBridgeShowdown } from './CentralBridgeShowdown.js';
 import { RuinsOfValor } from './RuinsOfValor.js';
 
 export const maps = {
-    GrasslandSkirmish,
+    // 수정: 키 이름을 GrasslandSkirmish로 하고, 값으로 grasslandSkirmishMap을 할당합니다.
+    GrasslandSkirmish: grasslandSkirmishMap, 
     CentralBridgeShowdown,
     RuinsOfValor,
 };
-
