69a5f549220972515f6b112353b74b6a6e95ca55
MSG7 <gimminseok1234@gmail.com>
2025-09-04 15:15:21 +0900

    Update index.js

--- js/maps/index.js
+++ js/maps/index.js
@@ -1,12 +1,12 @@
-// 이 파일에서 모든 기본 맵들을 불러와서 관리합니다.
-// 새로운 맵을 추가하려면, 아래에 import 구문을 추가하고 localMaps 배열에 맵 변수를 추가하기만 하면 됩니다.
+// js/maps/index.js
 
-import { grasslandSkirmishMap } from './GrasslandSkirmish.js';
-// '중앙대교 요새' 맵 추가 (가져오는 변수 이름을 수정해야 합니다)
-import { centralBridgeFortressMap } from './CentralBridgeShowdown.js';
+import { GrasslandSkirmish } from './GrasslandSkirmish.js';
+import { CentralBridgeShowdown } from './CentralBridgeShowdown.js';
+import { RuinsOfValor } from './RuinsOfValor.js';
 
-// 홈 화면에 표시할 기본 맵 목록
-export const localMaps = [
-    centralBridgeFortressMap, // 배열에 추가하는 변수 이름도 수정해야 합니다.
-    grasslandSkirmishMap,
-];
+export const maps = {
+    GrasslandSkirmish,
+    CentralBridgeShowdown,
+    RuinsOfValor,
+};
+
