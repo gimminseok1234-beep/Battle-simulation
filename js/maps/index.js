c2354aae523014361638f59c1decdf848da05d31
MSG7 <gimminseok1234@gmail.com>
2025-09-06 21:19:31 +0900

    Update index.js

--- js/maps/index.js
+++ js/maps/index.js
@@ -1,13 +1,13 @@
 // js/maps/index.js
 
-// 수정한 파일 이름과 변수명으로 데이터를 가져옵니다.
 import { grasslandMap } from './grassland.js';
 import { bridgeMap } from './bridge.js';
 import { ruinsMap } from './ruins.js';
+import { mazeMap } from './maze.js'; // 새로 추가된 미로 맵 import
 
-// localMaps 배열에 새로운 변수명을 사용합니다.
 export const localMaps = [
     grasslandMap,
     bridgeMap,
     ruinsMap,
+    mazeMap, // 배열에 미로 맵 추가
 ];
