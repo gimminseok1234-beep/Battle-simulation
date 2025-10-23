4281fadd78e5bf157ae9cb985f80cf9ee4e30356
MSG7 <gimminseok1234@gmail.com>
2025-10-23 20:12:04 +0900

    Update gameManager.render.js

--- js/gameManager.render.js
+++ js/gameManager.render.js
@@ -17,6 +17,7 @@ export function drawImpl(mouseEvent) {
     this.drawMap();
     this.magicCircles.forEach(c => c.draw(this.ctx));
     this.poisonClouds.forEach(c => c.draw(this.ctx));
+    this.poisonPuddles.forEach(p => p.draw(this.ctx)); // [신규] 독 장판 그리기
     
     if (this.state === 'SIMULATE' || this.state === 'PAUSED' || this.state === 'ENDING') {
         if (this.autoMagneticField.isActive) {
