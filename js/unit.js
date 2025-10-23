d0c598892649ef6be60f2ee1f18fdc93105f0540
MSG7 <gimminseok1234@gmail.com>
2025-10-23 14:30:35 +0900

    Update unit.js

--- js/unit.js
+++ js/unit.js
@@ -1491,8 +1491,7 @@ export class Unit {
         let specialSkillIsVisible =
             (this.weapon?.type === 'magic_dagger' && this.magicDaggerSkillCooldown > 0) ||
             (this.weapon?.type === 'axe' && this.axeSkillCooldown > 0) ||
-            (this.weapon?.type === 'ice_diamond' && this.iceDiamondChargeTimer > 0 && this.iceDiamondCharges < 5) ||
-            (this.weapon?.type === 'magic_spear' && this.magicCircleCooldown > 0) ||
+            (this.weapon?.type === 'ice_diamond' && this.iceDiamondChargeTimer > 0 && this.iceDiamondCharges < 5) || (this.weapon?.type === 'magic_spear' && this.magicSpearSpecialCooldown > 0) ||
             (this.weapon?.type === 'boomerang' && this.boomerangCooldown > 0) ||
             (this.weapon?.type === 'shuriken' && this.shurikenSkillCooldown > 0) ||
             (this.weapon?.type === 'fire_staff' && this.fireStaffSpecialCooldown > 0) ||
@@ -1590,9 +1589,9 @@ export class Unit {
             if (this.weapon?.type === 'fire_staff') {
                 fgColor = '#ef4444';
                 progress = 240 - this.fireStaffSpecialCooldown; max = 240;
-            } else if (this.weapon?.type === 'magic_spear') {
-                fgColor = '#a855f7';
-                progress = 300 - this.magicCircleCooldown; max = 300;
+            } else if (this.weapon?.type === 'magic_spear') { // [수정] 마법창 쿨다운 게이지 로직
+                fgColor = '#a855f7'; // 보라색
+                progress = 420 - this.magicSpearSpecialCooldown; max = 420; // 7초 (420 프레임)
             } else if (['boomerang', 'shuriken', 'poison_potion', 'magic_dagger', 'axe', 'dual_swords'].includes(this.weapon?.type)) {
                 fgColor = '#94a3b8';
                 if(this.weapon.type === 'boomerang') {
